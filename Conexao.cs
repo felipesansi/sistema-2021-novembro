a94f66c278f03f1907f5aba36ba20f56d549df45
felipesansi <82713076+felipesansi@users.noreply.github.com>
2021-11-23 15:16:06 -0300

    Add files via upload

---
+++ Conexao.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SistemaGestao.Base_de_dados
+{
+    class Conexao
+    {
+
+        SqlConnection conexao = new SqlConnection();
+
+
+        public Conexao()
+        {
+            conexao.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename=C:\Users\felip\source\repos\SistemaGestao2\bd_SistemaGeatao.mdf;Integrated Security=True";
+        }
+        public SqlConnection Conectar()
+        {
+            if (conexao.State == System.Data.ConnectionState.Closed)
+            {
+                conexao.Open();
+            }
+
+            return conexao;
+        }
+        public void desconectar()
+        {
+            if (conexao.State == System.Data.ConnectionState.Open)
+            {
+                conexao.Close();
+            }
+        }
+       
+    }
+}
